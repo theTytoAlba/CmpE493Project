c1173bf7ea2a145bb54a53ebdf2565f2fc3a2d20
Irmak Kavasoglu <irmak.kavasoglu@getir.com>
2018-04-08 19:32:42 +0300

    Added partial annotation

--- CmpE493Project/src/Main.java
+++ CmpE493Project/src/Main.java
@@ -5,12 +5,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	public static final int EXIT_CODE = 444;
+	public static final int NOT_ANNOTATED = 333;
 
 	public static void main(String[] args) {
-		// Read the tweets.
+		System.out.println("To save current annotation and exit, write " + EXIT_CODE + " during annotation.");
 		ArrayList<Tweet> tweets = new ArrayList<>();
+		// Check if there is a partially done annotation.
+		try {
+			Scanner in = new Scanner(new File("10_partial.txt"));
+			while (in.hasNextLine()) {
+				tweets.add(new Tweet(in.nextLine()));
+			}
+			System.out.println("Found partially done file. Collected " + tweets.size() + " tweets.");
+			in.close();
+		} catch (FileNotFoundException e) {}
+		
+		// Read the remaining tweets.
 		try {
 			Scanner in = new Scanner(new File("10.txt"));
+			// Do not re-read the already done tweets.
+			for (int i = 0; i < tweets.size(); i++) {
+				in.nextLine();
+			}
 			while (in.hasNextLine()) {
 				tweets.add(new Tweet(in.nextLine()));
 			}
@@ -22,18 +39,35 @@ public class Main {
 		
 		// Annotate the tweets.
 		Scanner in = new Scanner(System.in);
+		int annotatedLastTweet = -1;
 		for (int i = 0; i < tweets.size(); i++) {
+			// Skip the already annotated tweets.
+			if (tweets.get(i).annotation != NOT_ANNOTATED) {
+				continue;
+			}
 			System.out.println("Tweet " + (i+1) + "/" + tweets.size() + ":");
 			System.out.println(tweets.get(i).text);
-			tweets.get(i).annotation = in.nextInt();
+			int annotation = in.nextInt();
+			// Exit code. Ignore rest of the annotation and save what we have.
+			if (annotation == EXIT_CODE) {
+				annotatedLastTweet = i-1;
+				break;
+			} else {
+				tweets.get(i).annotation = annotation;
+				annotatedLastTweet = i;	
+			}
 		}
 		in.close();
 		
 		// Write the annotated tweets.
 		PrintWriter writer;
 		try {
-			writer = new PrintWriter("10_annotated.txt");
-			for (int i = 0; i < tweets.size(); i++) {
+			if (annotatedLastTweet == tweets.size()-1) {
+				writer = new PrintWriter("10_annotated.txt");	
+			} else {
+				writer = new PrintWriter("10_partial.txt");
+			}
+			for (int i = 0; i <= annotatedLastTweet; i++) {
 				writer.println(tweets.get(i).tweetToString());
 			}
 			writer.close();			
--- CmpE493Project/src/Tweet.java
+++ CmpE493Project/src/Tweet.java
@@ -4,8 +4,16 @@ public class Tweet {
 	String text;
 	int annotation;
 	public Tweet(String tweetLine) {
-		// lines are in form "\t<id>\t<tweet>".
+		// lines are in form "<annotation>\t<id>\t<tweet>".
 		String[] parts = tweetLine.split("\t");
+		try {
+			// Tweet may be already annotated.
+			annotation = Integer.parseInt(parts[0]);
+		} catch (Exception e) {
+			// If not, mark it with 333.
+			annotation = 333;
+		}
+		// Other values always exist.
 		id = parts[1];
 		text = parts[2];
 	}
